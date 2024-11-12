09ee840e3f61e6d52e4969434995b3449873bddc
CarlUdo <carl@wbsweden.com>
2024-11-12 12:10:22 +0100

    Make-mock-db
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/db/mock.ts
+++ next-js-chat/src/db/mock.ts
@@ -1,58 +1,58 @@
-const usersTable = [
-  {
-    id: 1,
-    username: "Anton",
-    tokens: 1
-  },
-  {
-    id: 2,
-    username: "Leo",
-    tokens: 2
-  },
-  {
-    id: 3,
-    username: "Carl",
-    tokens: 3
-  },
-];
+// export const usersTable = [
+//   {
+//     id: 1,
+//     username: "Anton",
+//     tokens: 1
+//   },
+//   {
+//     id: 2,
+//     username: "Leo",
+//     tokens: 2
+//   },
+//   {
+//     id: 3,
+//     username: "Carl",
+//     tokens: 3
+//   },
+// ];
 
-const messagesTable = [
-  {
-    id: 1,
-    content: "Hej alla!",
-    timpestamp: 1,
-    user_id: 1
-  },
-  {
-    id: 2,
-    content: "Lackar mot jul!",
-    timpestamp: 1,
-    user_id: 2
-  },
-  {
-    id: 3,
-    content: "Hej tomtegubbar!",
-    timpestamp: 1,
-    user_id: 3
-  },
-];
+// export const messagesTable = [
+//   {
+//     id: 1,
+//     content: "Hej alla!",
+//     timpestamp: 1,
+//     user_id: 1
+//   },
+//   {
+//     id: 2,
+//     content: "Lackar mot jul!",
+//     timpestamp: 1,
+//     user_id: 2
+//   },
+//   {
+//     id: 3,
+//     content: "Hej tomtegubbar!",
+//     timpestamp: 1,
+//     user_id: 3
+//   },
+// ];
 
-const fetchedMessages = [
-  {
-    id: 1,
-    timpestamp: 3,
-    user_id: 1
-  },
-  {
-    id: 2,
-    timpestamp: 1.5,
-    user_id: 2
-  },
-  {
-    id: 3,
-    timpestamp: 2,
-    user_id: 3
-  },
-];
+// export const fetchedMessages = [
+//   {
+//     id: 1,
+//     timpestamp: 3,
+//     user_id: 1
+//   },
+//   {
+//     id: 2,
+//     timpestamp: 1.5,
+//     user_id: 2
+//   },
+//   {
+//     id: 3,
+//     timpestamp: 2,
+//     user_id: 3
+//   },
+// ];
 
 
