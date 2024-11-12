037b3608130841fe7ea7cf69f0c7607d7adde0f6
CarlUdo <carl@wbsweden.com>
2024-11-12 12:33:43 +0100

    Update-repoitory
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/db/repository.ts
+++ next-js-chat/src/db/repository.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { messagesTable, usersTable } from './schema';
-import { eq } from 'drizzle-orm';
+import { fetchMessagesTable, messagesTable, usersTable } from './schema';
+import { eq, lte } from 'drizzle-orm';
 
 export const createRepository = () => {
   const db = drizzle(process.env.DATABASE_URL!);
@@ -13,6 +13,12 @@ export const createRepository = () => {
 
     getAllMessagesById: async (userId: string) => {
       return await db.select().from(messagesTable).where(eq(messagesTable.user_id, Number(userId)));
+    },
+
+    getAllMessagesByTimestamp: async (timestamp: number) => { 
+      return await db.select() 
+        .from(messagesTable) 
+        .where(lte(messagesTable.timestamp, timestamp)); 
     },
 
     postMessage: async (message: unknown) => {
@@ -27,7 +33,9 @@ export const createRepository = () => {
       await db.insert(usersTable).values(username);
     },
 
-
+    postFetchedMessage: async(userId: string, timestamp: number) => {
+      await db.insert(fetchMessagesTable).values({userId, timestamp});
+    }
   }
 
 
