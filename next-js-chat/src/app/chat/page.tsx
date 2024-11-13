c0884621be2b39374f7f4c7639b356df9a997c5b
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-13 12:27:36 +0100

    add-getfetchedmessagetimestamp-by-id
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/chat/page.tsx
+++ next-js-chat/src/app/chat/page.tsx
@@ -1,10 +1,13 @@
 import FetchMessageButton from "../../features/chat/fetch-message-button";
 //import MessageBoard from "../../../ui/message-board";
 import MessageInput from "../../../ui/message-input";
+import { chatService } from "@/features";
 
 export default async function Page() {
   //const messages = await createFeature().service.getAllMessages(id); // KOLLa använders sneaste fetch-token och hämta message. 
-
+  const timestamp = await chatService.getFetchedMessageTimestampById("1");
+  const messages = await chatService.getAllMessagesByTimestamp(timestamp);
+  console.log(messages);
   return (
     <>
         <MessageInput /> 
--- next-js-chat/src/features/chat/service.ts
+++ next-js-chat/src/features/chat/service.ts
@@ -1,16 +1,18 @@
 import { Db } from "@/db/instance";
 import { messagesTable, fetchMessagesTable, usersTable } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { eq, lte } from "drizzle-orm";
 
 export const createService = (db: Db) => {
   return {
     getAllMessages: async () => {
       return await db.select().from(messagesTable);
     },
-
+    getAllMessagesByTimestamp: async (timestamp: number) => {
+      return await db.select().from(messagesTable).where(lte(messagesTable.timestamp, timestamp));
+    },
     postMessage: async (message: string, userId: number) => {
       //zod valdiation::::. MÅSTE FIXA BTILL BIGINT eller så
-      const timestamp = 15//Date.now();
+      const timestamp = 13//Date.now();
 
       await db.insert(messagesTable).values({message, user_id: userId, timestamp});
     },
@@ -20,6 +22,10 @@ export const createService = (db: Db) => {
       const timestamp = 15//Date.now();
 
       await db.insert(fetchMessagesTable).values({user_id: Number(userId), timestamp});
+    },
+    getFetchedMessageTimestampById: async (userId: string) => {
+      const [fetchedMessage] = await db.select().from(fetchMessagesTable).where(eq(fetchMessagesTable.user_id, Number(userId)));
+      return fetchedMessage.timestamp;
     },
 
     updateUserTokenById: async (token: number, userId: number) => {
