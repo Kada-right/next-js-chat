f1d2b64ef4152d0f1f622432b14adf8f603310e8
CarlUdo <carl@wbsweden.com>
2024-11-13 14:13:57 +0100

    Make-new-service-functions
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/chat/page.tsx
+++ next-js-chat/src/app/chat/page.tsx
@@ -5,14 +5,14 @@ import MessageBoard from "@/features/chat/ui/message-board";
 
 export default async function Page() {
   const timestamp = await chatService.getFetchedMessageTimestampById("1");
-  const messages = await chatService.getAllMessagesByTimestamp(timestamp);
-
+  const cooldownMessages = await chatService.getAllMessagesByTimestampInCooldown(timestamp);  
+  const validMessages = await chatService.getAllValidMessagesByTimestamp(timestamp);
   return (
     <>
-        <MessageInput /> 
+      <MessageInput /> 
       <div>
-      <FetchMessageButton />
-      <MessageBoard messages={messages} /> 
+        <FetchMessageButton />
+        <MessageBoard messages={cooldownMessages} /> 
       </div> 
     </>
   );
