f4d4fa784037d72ed397f57765ae01af7c3634b4
CarlUdo <carl@wbsweden.com>
2024-11-13 12:52:37 +0100

    Move-components-and-call-service-functions
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/chat/page.tsx
+++ next-js-chat/src/app/chat/page.tsx
@@ -1,19 +1,18 @@
-import FetchMessageButton from "../../features/chat/fetch-message-button";
-//import MessageBoard from "../../../ui/message-board";
-import MessageInput from "../../../ui/message-input";
+import FetchMessageButton from "@/features/chat/ui/fetch-message-button";
 import { chatService } from "@/features";
+import MessageInput from "@/features/chat/ui/message-input";
+import MessageBoard from "@/features/chat/ui/message-board";
 
 export default async function Page() {
-  //const messages = await createFeature().service.getAllMessages(id); // KOLLa använders sneaste fetch-token och hämta message. 
   const timestamp = await chatService.getFetchedMessageTimestampById("1");
   const messages = await chatService.getAllMessagesByTimestamp(timestamp);
-  console.log(messages);
+
   return (
     <>
         <MessageInput /> 
       <div>
       <FetchMessageButton />
-{/*       <MessageBoard messages={messages} />  */}
+      <MessageBoard messages={messages} /> 
       </div> 
     </>
   );
