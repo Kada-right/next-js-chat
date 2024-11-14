2716536b6440ddc749b47013eba10b68b5de8e2e
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-14 17:30:52 +0100

    made-background-on-top-nav
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/dashboard/layout.tsx
+++ next-js-chat/src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import TopNav from "@/ui/dashboard/top-nav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="pr-24 pt-4">
+      <div className="pr-24 pt-4 bg-gradient-to-r from-gray-100 to-slate-500">
         <TopNav />
       </div>
       <div>{children}</div>
