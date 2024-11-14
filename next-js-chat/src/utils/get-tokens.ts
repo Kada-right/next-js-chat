e78c306af01ff05ce366ee25b539778f7e2a0fd8
CarlUdo <carl@wbsweden.com>
2024-11-14 10:29:31 +0100

    Make
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ next-js-chat/src/utils/get-tokens.ts
@@ -0,0 +1,23 @@
+import { getDaysBetweenTimestamps, isMonday, isTuesday } from "./date-helpers";
+
+export const getTokens = (tokens: number, timestamp: number, timeNow: number) => {
+   const numberOfDays = getDaysBetweenTimestamps(timestamp, timeNow);  
+
+  if (numberOfDays === 0) return tokens;
+
+  if (numberOfDays === 1) {
+    if (isMonday(timeNow)) {
+      return Math.min(3, tokens + 2); //tokens + 2 > 3 ? 3 : tokens + 2;
+    }
+    return Math.min(3, tokens + 1); //tokens + 1 > 3 ? 3 : tokens + 1;
+  }
+
+  if (numberOfDays === 2) {
+    if (isMonday(timeNow) || isTuesday(timeNow)) {
+      return 3;
+    }
+    return Math.min(3, tokens + 2); //tokens + 2 > 3 ? 3 : tokens + 2;
+  }
+  
+  return 3;
+};
