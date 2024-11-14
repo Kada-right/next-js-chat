1dca0758c9d08bf8db254b64ffaaee2b902e1e8b
CarlUdo <carl@wbsweden.com>
2024-11-14 13:23:31 +0100

    Make-stats-button
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ next-js-chat/src/features/stats/ui/stats-button.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Link from "next/link";
+
+export function StatsButton() {
+  const handleClick = () => {
+
+  };
+
+  return (
+    <button onClick={handleClick}> Stats </button>
+  );
+}
