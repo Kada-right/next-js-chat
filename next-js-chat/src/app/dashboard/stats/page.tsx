3d850baf7ba3f7027681d4ab6ddc830241a74fe1
CarlUdo <carl@wbsweden.com>
2024-11-14 15:55:35 +0100

    Make-stats
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/dashboard/stats/page.tsx
+++ next-js-chat/src/app/dashboard/stats/page.tsx
@@ -1,12 +1,10 @@
 import { chatService } from "@/features";
 
 async function getMessagesPerFetch(fetchValid: boolean) {
-
   let timestamp = await chatService.getLatestFetchedMessageTimestampById("1");
   let penultimateTimestamp = await chatService.getPenultimateFetchedMessageTimestampById("1");
 
-
-  if(fetchValid) {
+  if (fetchValid) {
       timestamp -= 1;
       penultimateTimestamp -= 1;
   }
@@ -14,18 +12,35 @@ async function getMessagesPerFetch(fetchValid: boolean) {
   const penultimateMessages = await chatService.getAllMessagesByTimestamp(penultimateTimestamp);
   const latestMessages = await chatService.getAllMessagesByTimestamp(timestamp);
   const messagesPerFetch = latestMessages.length - penultimateMessages.length;
+
   return messagesPerFetch;
 }
 
+async function getStats() {
+  const numberOfMessagesByUser = await chatService.getNumberOfMessagesByUserId("1");
+  const numberOfValidMessagesPerFetch = await getMessagesPerFetch(true);
+  const numberOfMessagesPerFetch = await getMessagesPerFetch(false);
+
+  return {
+    numberOfMessagesByUser,
+    numberOfValidMessagesPerFetch,
+    numberOfMessagesPerFetch
+  };
+}
 
 export default async function Page() {
-  const messagesPerFetch = await getMessagesPerFetch(true);
-
-  console.log(messagesPerFetch)
+  const {
+    numberOfMessagesByUser,
+    numberOfValidMessagesPerFetch,
+    numberOfMessagesPerFetch
+  } = await getStats();   
 
   return (
     <>
-    <p>Leaderboard</p>
+      <h1>Leaderboard</h1>
+      <p>Number of messages by user: {numberOfMessagesByUser}</p>
+      <p>Number of messages by fetch: {numberOfMessagesPerFetch}</p>
+      <p>Number of valid messages per fetch: {numberOfValidMessagesPerFetch}</p>      
     </>
   );
 }
