e704f6228fb07c024d01dd5e54b3ec2352d83684
CarlUdo <carl@wbsweden.com>
2024-11-14 13:23:04 +0100

    Change-to-equal-conditions
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/tests/logic.test.ts
+++ next-js-chat/src/tests/logic.test.ts
@@ -1,5 +1,5 @@
 import { getTokens } from "@/utils/get-tokens";
-import { deepEqual } from "node:assert/strict";
+import { equal } from "node:assert/strict";
 import test from "node:test";
 
 test("should add 2 tokens if 2 days have passed and none of them is a Sunday", () => {
@@ -7,7 +7,7 @@ test("should add 2 tokens if 2 days have passed and none of them is a Sunday", (
     const ts2 = new Date("2023-11-12T12:00:00").getTime();
     const token = 0;
     const result = getTokens(token, ts1, ts2);
-    deepEqual(result, 2);
+    equal(result, 2);
 });
 
 test("should add 3 tokens if 2 days have passed and one of the days is a Sunday", () => {
@@ -15,7 +15,7 @@ test("should add 3 tokens if 2 days have passed and one of the days is a Sunday"
   const ts2 = new Date("2023-11-13T12:00:00").getTime();
   const token = 0;
   const result = getTokens(token, ts1, ts2);
-  deepEqual(result, 3);
+  equal(result, 3);
 });
 
 test("should add 2 tokens if 1 days have passed and that day is a Sunday", () => {
@@ -23,7 +23,7 @@ test("should add 2 tokens if 1 days have passed and that day is a Sunday", () =>
   const ts2 = new Date("2023-11-13T12:00:00").getTime();
   const token = 0;
   const result = getTokens(token, ts1, ts2);
-  deepEqual(result, 2);
+  equal(result, 2);
 });
 
 test("should add 1 token if 1 day has passed and that day is not a Sunday", () => {
@@ -31,7 +31,7 @@ test("should add 1 token if 1 day has passed and that day is not a Sunday", () =
   const ts2 = new Date("2024-11-14T12:00:00").getTime();
   const token = 0;
   const result = getTokens(token, ts1, ts2);
-  deepEqual(result, 1);
+  equal(result, 1);
 });
 
 test("should add 3 tokens if 5 days have passed", () => {
@@ -39,7 +39,7 @@ test("should add 3 tokens if 5 days have passed", () => {
     const ts2 = new Date("2024-11-14T12:00:00").getTime();
     const token = 0;
     const result = getTokens(token, ts1, ts2);
-    deepEqual(result, 3);
+    equal(result, 3);
 });
 
 test("should add 0 tokens if on same day", () => {
@@ -47,7 +47,7 @@ test("should add 0 tokens if on same day", () => {
   const ts2 = new Date("2024-11-09T12:00:00").getTime();
   const token = 0;
   const result = getTokens(token, ts1, ts2);
-  deepEqual(result, 0);
+  equal(result, 0);
 });
 
 test("should add 0 tokens if there is already 3 tokens and two days have passed", () => {
@@ -55,5 +55,5 @@ test("should add 0 tokens if there is already 3 tokens and two days have passed"
   const ts2 = new Date("2024-11-14T12:00:00").getTime();
   const token = 3;
   const result = getTokens(token, ts1, ts2);
-  deepEqual(result, 3);
+  equal(result, 3);
 });
