8947c7244bc3755b23c5d911134ddb3f21b61f45
CarlUdo <carl@wbsweden.com>
2024-11-14 13:27:56 +0100

    Make-dashboard-layout
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ next-js-chat/src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import TopNav from "../../../ui/dashboard/top-nav";
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+    <div>
+      <TopNav />
+    </div>
+      <div>
+        {children}
+      </div>
+    </>      
+  );
+}
