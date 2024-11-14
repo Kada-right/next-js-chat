dbe9f4d9e8d74a6389393ae994f5627bab49b601
CarlUdo <carl@wbsweden.com>
2024-11-14 17:14:47 +0100

    Update-with
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/dashboard/chat/page.tsx
+++ next-js-chat/src/app/dashboard/chat/page.tsx
@@ -1,21 +1,43 @@
 import { chatService } from "@/features";
 import MessageInput from "@/features/chat/ui/message-input";
 import MessageBoard from "@/features/chat/ui/message-board";
+type CooldownMessages = {
+  id: number;
+  message: string;
+  timestamp: number;
+  user_id: number;
+}[];
+
+type validMessages = {
+  id: number;
+  timestamp: number;
+  user_id: number;
+  message: string;
+}[];
 
 export default async function Page() {
   const timestamp = await chatService.getLatestFetchedMessageTimestampById("1");
-  const cooldownMessages =
-    await chatService.getAllMessagesByTimestampInCooldown(timestamp);
-  const validMessages =
-    await chatService.getAllValidMessagesByTimestamp(timestamp);
+
+  let cooldownMessages: CooldownMessages = [];
+  let validMessages: validMessages = [];
+
+  if (timestamp) {
+    cooldownMessages =
+      await chatService.getAllMessagesByTimestampInCooldown(timestamp);
+    validMessages =
+      await chatService.getAllValidMessagesByTimestamp(timestamp);
+  }
 
   return (
     <div className="w-full max-w-screen-lg mx-auto  border-t-4 border-r-4 border-slate-500 rounded-lg mt-5">
-      <div className="flex flex-col items-center w-full h-full">
-        <MessageBoard
-          validMessage={validMessages}
-          cooldownMessages={cooldownMessages}
-        />
+      <div className="bg-gradient-to-r from-gray-100 to-slate-200 flex flex-col items-center w-full h-full">
+        {
+          timestamp &&
+          <MessageBoard
+            validMessage={validMessages}
+            cooldownMessages={cooldownMessages}
+          />
+        }
         <MessageInput />
       </div>
     </div>
