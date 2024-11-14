6d4af123375e307dcecf28b1dd2ea673af2523af
CarlUdo <carl@wbsweden.com>
2024-11-14 16:15:11 +0100

    Update-with-link
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/page.tsx
+++ next-js-chat/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="container mx-auto border-4 border-red-500">
       <p>Hello from main page</p>
-      <div>Login</div>
+      <a href="/dashboard/chat">Login</a>
     </div>
   );
 }
