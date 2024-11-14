37c4c1278b198d6e8f0175ae16a29b717b7bdf3a
CarlUdo <carl@wbsweden.com>
2024-11-14 13:27:23 +0100

    Make-dashboard-page
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ next-js-chat/src/app/dashboard/stats/page.tsx
@@ -0,0 +1,19 @@
+import FetchMessageButton from "@/features/chat/ui/fetch-message-button";
+import { chatService } from "@/features";
+import MessageInput from "@/features/chat/ui/message-input";
+import MessageBoard from "@/features/chat/ui/message-board";
+
+export default async function Page() {
+  const timestamp = await chatService.getFetchedMessageTimestampById("1");
+  const cooldownMessages = await chatService.getAllMessagesByTimestampInCooldown(timestamp);  
+  const validMessages = await chatService.getAllValidMessagesByTimestamp(timestamp);
+  return (
+    <>
+      <MessageInput /> 
+      <div>
+        <FetchMessageButton />
+        <MessageBoard validMessage={validMessages} cooldownMessages={cooldownMessages} /> 
+      </div> 
+    </>
+  );
+}
