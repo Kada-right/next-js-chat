5fe837dea404ff49fdcfc732b4502b245c11b5eb
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-14 17:07:57 +0100

    switched-apostrophy-to-not-get-error
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/page.tsx
+++ next-js-chat/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-gray-100 to-slate-500">
       <div className="max-w-lg w-full bg-white rounded-lg shadow-lg p-8 border-t-4 border-r-4 border-slate-500 text-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Welcome to the World's Slowest Chat
+          Welcome to the World`s Slowest Chat
         </h1>
         <p className="text-gray-600 text-lg mb-6">
           Where every message is worth the wait!
