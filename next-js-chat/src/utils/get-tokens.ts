70eeb6bb9e83f417bc900238a07cde143037a1e1
CarlUdo <carl@wbsweden.com>
2024-11-14 13:22:17 +0100

    Update-get-tokens-function
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/utils/get-tokens.ts
+++ next-js-chat/src/utils/get-tokens.ts
@@ -3,20 +3,22 @@ import { getDaysBetweenTimestamps, isMonday, isTuesday } from "./date-helpers";
 export const getTokens = (tokens: number, timestamp: number, timeNow: number) => {
    const numberOfDays = getDaysBetweenTimestamps(timestamp, timeNow);  
 
+   //gör test för -dagar...
+
   if (numberOfDays === 0) return tokens;
 
   if (numberOfDays === 1) {
     if (isMonday(timeNow)) {
-      return Math.min(3, tokens + 2); //tokens + 2 > 3 ? 3 : tokens + 2;
+      return Math.min(3, tokens + 2);
     }
-    return Math.min(3, tokens + 1); //tokens + 1 > 3 ? 3 : tokens + 1;
+    return Math.min(3, tokens + 1); 
   }
 
   if (numberOfDays === 2) {
     if (isMonday(timeNow) || isTuesday(timeNow)) {
       return 3;
     }
-    return Math.min(3, tokens + 2); //tokens + 2 > 3 ? 3 : tokens + 2;
+    return Math.min(3, tokens + 2); 
   }
   
   return 3;
--- next-js-chat/ui/stats.tsx
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-  return (
-    <button>
-      Stats
-    </button>
-  );
-}
