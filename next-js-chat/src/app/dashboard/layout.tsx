dba2f7436c3ba42ee67c34ed7e9ca0f2cf32415c
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-14 17:09:23 +0100

    made-nav-changes
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/dashboard/layout.tsx
+++ next-js-chat/src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import TopNav from "@/ui/dashboard/top-nav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div>
+      <div className="pr-24 pt-4">
         <TopNav />
       </div>
       <div>{children}</div>
