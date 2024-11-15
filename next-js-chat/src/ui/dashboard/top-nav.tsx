04cc7ce15777565413b1f6143d7685c4d493f21d
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-15 08:30:05 +0100

    updated-nav
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/ui/dashboard/top-nav.tsx
+++ next-js-chat/src/ui/dashboard/top-nav.tsx
@@ -1,15 +1,5 @@
 import { NavLinks } from "./nav-links";
 
 export default function TopNav() {
-  return (
-    <div>
-      <NavLinks />
-      {/* <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form> */}
-    </div>
-  );
+  return <NavLinks />
 }
