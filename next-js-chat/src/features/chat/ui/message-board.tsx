1208c5557236a45cc78c3ef8a25148a4428ef609
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-15 09:56:34 +0100

    add-back-props-from-types
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/features/chat/ui/message-board.tsx
+++ next-js-chat/src/features/chat/ui/message-board.tsx
@@ -1,6 +1,20 @@
-import { MessageBoardProps } from "../types";
 import { CooldownMessage } from "./cooldown-message";
 import { ValidMessage } from "./valid-message";
+
+type MessageBoardProps = {
+  cooldownMessages: {
+    id: number;
+    message: string;
+    timestamp: number;
+    user_id: number;
+  }[];
+  validMessage: {
+    id: number;
+    message: string;
+    timestamp: number;
+    user_id: number;
+  }[];
+};
 
 export default function MessageBoard({
   validMessage,
