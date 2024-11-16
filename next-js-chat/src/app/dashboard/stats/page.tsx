0768ab7ae132c0f1868ede6115956d3f175bcb82
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-16 15:16:48 +0100

    changed-stats-message
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/dashboard/stats/page.tsx
+++ next-js-chat/src/app/dashboard/stats/page.tsx
@@ -6,8 +6,8 @@ async function getMessagesPerFetch(fetchValid: boolean) {
     await chatService.getPenultimateFetchedMessageTimestampById("1");
 
   if (fetchValid) {
-    timestamp -= 1;
-    penultimateTimestamp -= 1;
+    timestamp -= (1000 * 60);
+    penultimateTimestamp -= (1000 * 60);
   }
 
   const penultimateMessages =
@@ -15,19 +15,23 @@ async function getMessagesPerFetch(fetchValid: boolean) {
   const latestMessages = await chatService.getAllMessagesByTimestamp(timestamp);
   const messagesPerFetch = latestMessages.length - penultimateMessages.length;
 
-  return messagesPerFetch;
+  return {messagesPerFetch, timestamp};
 }
 
 async function getStats() {
   const numberOfMessagesByUser =
     await chatService.getNumberOfMessagesByUserId("1");
-  const numberOfValidMessagesPerFetch = await getMessagesPerFetch(true);
-  const numberOfMessagesPerFetch = await getMessagesPerFetch(false);
+  const data = await getMessagesPerFetch(true);
+  const numberOfValidMessagesPerFetch = data.messagesPerFetch;
+  const validTimestamp = data.timestamp;
+  const numberOfMessagesPerFetch = (await getMessagesPerFetch(false)).messagesPerFetch;
+  
 
   return {
     numberOfMessagesByUser,
     numberOfValidMessagesPerFetch,
     numberOfMessagesPerFetch,
+    validTimestamp
   };
 }
 
@@ -36,6 +40,8 @@ export default async function Page() {
     numberOfMessagesByUser,
     numberOfValidMessagesPerFetch,
     numberOfMessagesPerFetch,
+    validTimestamp
+
   } = await getStats();
 
   return (
@@ -43,18 +49,17 @@ export default async function Page() {
       <div className="w-full max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg text-center">
         <h1 className="text-2xl font-bold mb-4 text-gray-800">Leaderboard</h1>
         <p className="text-lg text-gray-700 mb-2">
-          <span className="font-semibold">Number of written messages by user:</span>{" "}
-          {numberOfMessagesByUser}
+          <span className="font-semibold">The user has written a total of {numberOfMessagesByUser} messages</span>{" "}
         </p>
         <p className="text-lg text-gray-700 mb-2">
-          <span className="font-semibold">Number of messages per fetch:</span>{" "}
-          {numberOfMessagesPerFetch}
+        <span className="font-semibold">
+        As of {`${new Date(validTimestamp).toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' })} on ${new Date(validTimestamp).toISOString().split('T')[0]}`}, <br/> {numberOfMessagesPerFetch} new message(s) were recorded
+</span>
         </p>
         <p className="text-lg text-gray-700 mb-2">
-          <span className="font-semibold">
-          Number of new messages per fetch:
-          </span>{" "}
-          {numberOfValidMessagesPerFetch}
+        <span className="font-semibold">
+        Of the new message(s) recorded {numberOfValidMessagesPerFetch} were valid
+</span>
         </p>
       </div>
     </div>
