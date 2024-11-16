018e6aaf50bfb93dce151fb1ab3b637b1a6088e9
CarlUdo <carl@wbsweden.com>
2024-11-16 15:41:17 +0100

    Update-sentences
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/app/dashboard/stats/page.tsx
+++ next-js-chat/src/app/dashboard/stats/page.tsx
@@ -43,26 +43,30 @@ export default async function Page() {
     validTimestamp,
   } = await getStats();
 
+  const messagesTextUser = numberOfMessagesByUser > 1 || numberOfMessagesByUser === 0 ? "messages" : "message";
+  const messagesTextFetch = numberOfMessagesPerFetch > 1 || numberOfMessagesPerFetch === 0 ? "messages" : "message";
+  const wasWereTextFetch = numberOfMessagesPerFetch > 1 || numberOfMessagesPerFetch === 0 ? "were" : "was";
+  const wasWereTextValid = numberOfValidMessagesPerFetch > 1 || numberOfValidMessagesPerFetch === 0  ? "were" : "was";
+
   return (
     <div className="h-screen bg-gradient-to-r from-gray-100 to-slate-500 flex items-center justify-center">
       <div className="w-full max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg text-center">
         <h1 className="text-2xl font-bold mb-4 text-gray-800">Leaderboard</h1>
         <p className="text-lg text-gray-700 mb-2">
           <span className="font-semibold">
-            The user has written a total of {numberOfMessagesByUser} messages
+            The user has written a total of,<br /> {numberOfMessagesByUser} {messagesTextUser}.
           </span>{" "}
         </p>
         <p className="text-lg text-gray-700 mb-2">
           <span className="font-semibold">
             As of{" "}
             {`${new Date(validTimestamp).toLocaleTimeString("en-US", { hour12: false, hour: "2-digit", minute: "2-digit" })} on ${new Date(validTimestamp).toISOString().split("T")[0]}`}
-            , <br /> {numberOfMessagesPerFetch} new message(s) were recorded
+            , <br /> {numberOfMessagesPerFetch} new {messagesTextFetch} {wasWereTextFetch} recorded.
           </span>
         </p>
         <p className="text-lg text-gray-700 mb-2">
           <span className="font-semibold">
-            Of the new message(s) recorded {numberOfValidMessagesPerFetch} were
-            valid
+            Of the new {messagesTextFetch} recorded,<br /> {numberOfValidMessagesPerFetch} {wasWereTextValid} not on cooldown.
           </span>
         </p>
       </div>
