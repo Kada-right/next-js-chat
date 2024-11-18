91d3b1408fd303fc06b2eb57c15aa50413050667
CarlUdo <carl@wbsweden.com>
2024-11-18 13:41:58 +0100

    Run-prettier
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/ui/design-system/left-nav.tsx
+++ next-js-chat/src/ui/design-system/left-nav.tsx
@@ -1,100 +1,61 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
-import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+
+import CustomLink from "../custom-link";
+import { useToggleSections } from "@/utils/hooks";
+import NavBarButton from "../nav-bar-button";
 export function NavLinks() {
-  const [hideFoundation, setHideFoundation] = useState(true);
-  const [hideComponent, setHideComponent] = useState(true);
-  const [hidePatterns, setHidePatterns] = useState(true);
-  const toggleFoundationButton = () => {
-    setHideFoundation(!hideFoundation);
-  };
-  const toggleComponentsButton = () => {
-    setHideComponent(!hideComponent);
-  };
-  const togglePatternsButton = () => {
-    setHidePatterns(!hidePatterns);
-  };
-
-  type CustomLinkprops = {
-    href: string; 
-    name: string;
-    isHidden: boolean;
-  }
-
-  const CustomLink = ({ href, name, isHidden }: CustomLinkprops) => {
-    return (
-      <Link 
-        className="pl-6 hover:bg-slate-300 w-full py-1" 
-        href={href}
-        hidden={isHidden}
-      >
-        {name}
-      </Link>
-    );
-  };
-
+  const { hiddenStates, toggleSection } = useToggleSections();
 
   return (
     <div className="bg-slate-400 w-40 flex flex-col items-start border-2 border-red-600">
-      <button
-        className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
-        onClick={toggleFoundationButton}
-      >
-        <span className="pl-2">Foundations</span>
-        {hideFoundation ? <IoIosArrowForward /> : <IoIosArrowDown />}
-      </button>
+      <NavBarButton
+        onclick={toggleSection}
+        folderSection="foundations"
+        name="Foundations"
+        isHidden={hiddenStates.foundations}
+      />
       <div className="flex flex-col w-full">
-        <CustomLink 
+        <CustomLink
           href="/design-system/colors"
           name="Colors"
-          isHidden={hideFoundation}
-
+          isHidden={hiddenStates.foundations}
         />
-        <CustomLink 
+        <CustomLink
           href="/design-system/layouts"
           name="Layouts"
-          isHidden={hideFoundation}
-
+          isHidden={hiddenStates.foundations}
         />
-        <CustomLink 
+        <CustomLink
           href="/design-system/typography"
           name="Typography"
-          isHidden={hideFoundation}
-
+          isHidden={hiddenStates.foundations}
         />
       </div>
-      <button
-        className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
-        onClick={toggleComponentsButton}
-      >
-        <span className="pl-2">Components</span>
-        {hideComponent ? <IoIosArrowForward /> : <IoIosArrowDown />}
-      </button>
+      <NavBarButton
+        onclick={toggleSection}
+        folderSection="components"
+        name="Components"
+        isHidden={hiddenStates.components}
+      />
       <div className="flex flex-col w-full">
-        <Link
-          className="pl-6 hover:bg-slate-300 w-full py-1"
-          hidden={hideComponent}
-          href={"/design-system/buttons"}
-        >
-          Buttons
-        </Link>
+        <CustomLink
+          href="/design-system/buttons"
+          name="Buttons"
+          isHidden={hiddenStates.components}
+        />
       </div>
-      <button
-        className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
-        onClick={togglePatternsButton}
-      >
-        <span className="pl-2">Patterns</span>
-        {hidePatterns ? <IoIosArrowForward /> : <IoIosArrowDown />}
-      </button>
+      <NavBarButton
+        onclick={toggleSection}
+        folderSection="patterns"
+        name="Patterns"
+        isHidden={hiddenStates.patterns}
+      />
       <div className="flex flex-col w-full">
-        <Link
-          className="pl-6 hover:bg-slate-300 w-full py-1"
-          hidden={hidePatterns}
-          href={"/design-system/forms"}
-        >
-          Forms
-        </Link>
+        <CustomLink
+          href="/design-system/forms"
+          name="Forms"
+          isHidden={hiddenStates.patterns}
+        />
       </div>
     </div>
   );
