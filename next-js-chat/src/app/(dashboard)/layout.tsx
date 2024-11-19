e7667f6ce09632f4eb8dcb862e63e53572608a75
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:29:27 +0100

    updated-title
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/(dashboard)/layout.tsx
+++ next-js-chat/src/app/(dashboard)/layout.tsx
@@ -2,9 +2,9 @@ import TopNav from "@/ui/dashboard/top-nav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main>
       <TopNav />
       {children}
-    </>
+    </main>
   );
 }
