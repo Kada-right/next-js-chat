2481b159823ef08b64e05b92ed6e2f490d4ea44a
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-19 14:56:54 +0100

    removed-comments-and-switched-name
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/design-system/layout.tsx
+++ next-js-chat/src/app/design-system/layout.tsx
@@ -1,16 +1,12 @@
-// components/Layout.js
+import { LeftNav } from "@/ui/design-system/left-nav";
+import { ReactNode } from "react";
 
-import { NavLinks } from "@/ui/design-system/left-nav";
-
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
-      {/* Sidebar */}
       <aside className="w-[240px] bg-slate-400 h-screen sticky top-0 overflow-y-auto flex-shrink-0">
-        <NavLinks />
+        <LeftNav />
       </aside>
-
-      {/* Main Content */}
       <main className="flex-grow p-4">{children}</main>
     </div>
   );
