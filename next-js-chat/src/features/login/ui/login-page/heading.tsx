1b677ec7bc74a24271f897df1f07b326251976a4
CarlUdo <carl@wbsweden.com>
2024-11-19 14:16:01 +0100

    Run-prettier
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- next-js-chat/src/features/login/ui/login-page/heading.tsx
+++ next-js-chat/src/features/login/ui/login-page/heading.tsx
@@ -1,7 +1,3 @@
 export function Heading({ title }: { title: string }) {
-  return (
-    <h1 className="text-3xl font-bold text-gray-800 mb-4">
-      {title}
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold text-gray-800 mb-4">{title}</h1>;
 }
