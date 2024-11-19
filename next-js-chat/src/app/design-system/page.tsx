89866f96422134e8af072f21f6746cd8c3286f13
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-19 16:45:23 +0100

    created-a-front-page
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/design-system/page.tsx
+++ next-js-chat/src/app/design-system/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Page?</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center  px-4 py-6">
+      <h1 className="font-bold text-2xl text-center mb-2">
+        Welcome to the Design-System of Our Chat App
+      </h1>
+      <p className="text-center text-gray-600">
+        Use the navigation menu on the left to explore our design system.
+      </p>
+    </div>
+  );
 }
