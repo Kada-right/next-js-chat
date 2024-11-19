e867a1a2dfa23d15c2104b0e2e757a733cf6758e
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-19 14:23:44 +0100

    made-login-page
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/page.tsx
+++ next-js-chat/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { LoginWindow } from "@/features/login/ui/login-page/login-window";
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-slate-200 b">
-      <LoginWindow />
-    </div>
-  );
-}
+import { LoginPage } from "@/features/login/ui/login-page/login-page";
+  export default LoginPage;
+  
