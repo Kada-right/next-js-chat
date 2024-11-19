2b1875c1ee75693cbccdf79a937cd173a2b90807
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:25:24 +0100

    moved-div-to-inner-messageboard
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/features/chat/ui/chat-page.tsx
+++ next-js-chat/src/features/chat/ui/chat-page.tsx
@@ -1,19 +1,11 @@
-import { chatService } from "@/features";
 import MessageInput from "@/features/chat/ui/message-input";
 import MessageBoard from "@/features/chat/ui/message-board";
 
 export async function ChatPage() {
-
-  const tokens = await chatService.getTokensById("1");
-
-  return(
-      <div className="w-full max-w-screen-lg mx-auto border-t-2 border-r-2 border-b-2 border-l-2 border-slate-500 rounded-lg shadow-lg">
-        <div className="flex flex-col items-center w-full h-full">
-          <div className="w-full h-96 overflow-y-auto">
-            <MessageBoard/>
-          </div>
-          <MessageInput tokens={tokens} />
-        </div>
+  return (
+    <div className="w-full max-w-screen-lg mx-auto border-t-2 border-r-2 border-b-2 border-l-2 border-slate-500 rounded-lg shadow-lg">
+          <MessageBoard />
+        <MessageInput />
       </div>
   );
 }
