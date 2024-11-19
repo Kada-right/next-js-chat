1260a74bade5689750f7814c91595aa0ac518ebc
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:24:58 +0100

    made-form-comp
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ next-js-chat/src/features/chat/ui/form.tsx
@@ -0,0 +1,28 @@
+import RegularButton from "@/ui/design-system/buttons/regular-button";
+import { postMessageAction } from "..";
+
+export async function Form() {
+  return (
+    <form action={postMessageAction}>
+      <div>
+        <label
+          htmlFor="message"
+          className="block text-sm font-medium text-gray-300 mb-2"
+        >
+          Meddelande:
+        </label>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          required
+          className="w-full p-2 border border-gray-300 rounded-md"
+          placeholder="Skriv ditt meddelande här..."
+        />
+      </div>
+      <div className="pt-2 flex justify-center">
+        <RegularButton name="Send message" />
+      </div>
+    </form>
+  );
+}
--- next-js-chat/src/features/chat/ui/message-input.tsx
+++ next-js-chat/src/features/chat/ui/message-input.tsx
@@ -1,37 +1,13 @@
-import { postMessageAction } from "@/features/chat/actions";
 import FetchMessageButton from "./fetch-message-button";
-import RegularButton from "@/ui/design-system/buttons/regular-button";
+import { chatService } from "../instance";
+import { Form } from "./form";
 
-export default function MessageInput({ tokens }: { tokens: number }) {
+export default async function MessageInput() {
+  const tokens = await chatService.getTokensById("1");
+
   return (
     <div className="w-full shadow-lg rounded-lg mt-0 p-5">
-      <form action={postMessageAction}>
-        <div>
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-300 mb-2"
-          >
-            Meddelande:
-          </label>
-          <input
-            type="text"
-            id="message"
-            name="message"
-            required
-            className="w-full p-2 border border-gray-300 rounded-md"
-            placeholder="Skriv ditt meddelande här..."
-          />
-        </div>
-        {/*         <button
-          className="bg-slate-500 mt-4 px-4 py-2 rounded-md text-white font-medium mx-auto block transition-transform transform hover:bg-slate-6  00 hover:scale-105"
-          type="submit"
-        >
-          Send message
-        </button> */}
-        <div className="pt-2 flex justify-center">
-          <RegularButton name="Send message" />
-        </div>
-      </form>
+      <Form />
       <div className="pt-2 flex justify-center">
         <FetchMessageButton />
       </div>
