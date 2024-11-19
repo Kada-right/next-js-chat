71e97b26b8a97d771aaaff48dd2bcb7c6284fab0
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-19 16:07:02 +0100

    changed-name
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ next-js-chat/src/ui/group-link.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import CustomLink from "./custom-link";
+import { DesignLink } from "@/utils/types";
+import { useState } from "react";
+
+
+type Props = {
+  name: "foundations" | "components" | "patterns";
+  designLinks: DesignLink[]
+};
+
+export default function GroupLink({
+  name,
+  designLinks
+}: Props) {
+const [isHidden, setIsHidden] = useState(true);
+  return (
+    <>
+      <button
+        className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
+        onClick={() => setIsHidden(!isHidden)}
+      >
+        {name}
+        {isHidden ? <IoIosArrowForward /> : <IoIosArrowDown />}
+      </button>
+      <div className="flex flex-col w-full">
+      {designLinks.map((link, index) => <CustomLink key={index} href={link.href} name={link.name} isHidden={isHidden}  />)}
+      </div>
+    </>
+  );
+}
