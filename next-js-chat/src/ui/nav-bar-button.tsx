86563d900c4ecfb49166ca58c4723cef6527ac19
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-11-19 14:57:12 +0100

    started-spliting-customlinks
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/ui/nav-bar-button.tsx
+++ next-js-chat/src/ui/nav-bar-button.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import type { FolderSection } from "@/utils/hooks";
+import { useToggleSections, type FolderSection } from "@/utils/hooks";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import CustomLink from "./custom-link";
+import { Index } from "drizzle-orm/mysql-core";
 
 type Props = {
   folderSection: FolderSection;
@@ -10,20 +12,40 @@ type Props = {
   onclick: (section: FolderSection) => void;
   isHidden: boolean;
 };
-
+const designLinks = [
+  {
+    href: "/design-system/colors",
+    name: "colors",
+  },
+  {
+    href: "/design-system/layouts",
+    name: "layouts",
+  },
+  {
+    href: "/design-system/typography",
+    name: "typography",
+  },
+];
 export default function NavBarButton({
   folderSection,
   name,
   isHidden,
   onclick,
 }: Props) {
+  const { hiddenStates, toggleSection } = useToggleSections();
+
   return (
-    <button
-      className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
-      onClick={() => onclick(folderSection)}
-    >
-      {name}
-      {isHidden ? <IoIosArrowForward /> : <IoIosArrowDown />}
-    </button>
+    <>
+      <button
+        className="hover:bg-slate-300 font-bold flex w-full items-center justify-between pl-2 pr-2 py-2"
+        onClick={() => onclick(folderSection)}
+      >
+        {name}
+        {isHidden ? <IoIosArrowForward /> : <IoIosArrowDown />}
+      </button>
+      <div className="flex flex-col w-full">
+      {designLinks.map((link, index) => <CustomLink key={index} href={link.href} name={link.name} isHidden={false}  />)}
+      </div>
+    </>
   );
 }
