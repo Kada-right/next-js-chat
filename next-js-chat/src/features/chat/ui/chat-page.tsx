cdf694f291b1d418ccaf71077225347100c13c35
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:26:00 +0100

    format
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/features/chat/ui/chat-page.tsx
+++ next-js-chat/src/features/chat/ui/chat-page.tsx
@@ -4,8 +4,8 @@ import MessageBoard from "@/features/chat/ui/message-board";
 export async function ChatPage() {
   return (
     <div className="w-full max-w-screen-lg mx-auto border-t-2 border-r-2 border-b-2 border-l-2 border-slate-500 rounded-lg shadow-lg">
-          <MessageBoard />
-        <MessageInput />
-      </div>
+      <MessageBoard />
+      <MessageInput />
+    </div>
   );
 }
