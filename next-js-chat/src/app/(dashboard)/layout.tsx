602a63d7e241465707a5f49d6737cb5cdda71cd0
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:13:54 +0100

    removed-unnecessary-div
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- next-js-chat/src/app/(dashboard)/layout.tsx
+++ next-js-chat/src/app/(dashboard)/layout.tsx
@@ -3,10 +3,8 @@ import TopNav from "@/ui/dashboard/top-nav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="pr-24 pt-4 bg-gradient-to-r from-gray-100 to-slate-500">
         <TopNav />
-      </div>
-      <div>{children}</div>
+        {children}
     </>
   );
 }
