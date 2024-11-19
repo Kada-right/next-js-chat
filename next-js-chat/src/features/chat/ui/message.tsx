d23379fc13cf81341ff1f1e795569199632f8489
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-19 12:13:10 +0100

    made-general-message-comp
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

---
+++ next-js-chat/src/features/chat/ui/message.tsx
@@ -0,0 +1,28 @@
+import { chatService } from "../instance";
+
+type Props = {
+  sentMessage: {
+    id: number;
+    message: string;
+    timestamp: number;
+    user_id: number;
+  };
+  isValid: boolean;
+};
+
+export async function Message({ sentMessage, isValid }: Props) {
+  const name = await chatService.getUserNameById(sentMessage.user_id);
+  const timestamp = new Date(sentMessage.timestamp);
+  const messageToShow = isValid ? `Message: ${sentMessage.message}` : "Cooldown message";
+
+  return (
+    <>
+      <p>{messageToShow}</p>
+      <p>
+        Date:{" "}
+        {`${timestamp.toLocaleDateString()} ${timestamp.toLocaleTimeString()}`}
+      </p>
+      <p>Name: {name}</p>
+    </>
+  );
+}
